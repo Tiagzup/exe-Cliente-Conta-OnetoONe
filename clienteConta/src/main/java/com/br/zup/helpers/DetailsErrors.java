68bda7d3229d0159004a1c2c8531df4bed8afca2
Tiago Souza <tiago.souza@zup.com.br>
2019-07-25 20:59:45 -0300

    Criar classe de modelo de erros

---
+++ clienteConta/src/main/java/com/br/zup/helpers/DetailsErrors.java
@@ -0,0 +1,40 @@
+package com.br.zup.helpers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+
+public class DetailsErrors {
+	
+	private Date date;
+	private int status;
+	private String message;
+	private String origin;
+	
+	public DetailsErrors(HttpStatus status, String message, String origin) {
+		super();
+		this.date = new Date ();
+		this.status = status.value();
+		this.message = message;
+		this.origin = origin;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+	
+
+	
+}
